0e3f2b9664a2144493ff16ba643ff6fac00d1837
Marc Costello <cossie7722@gmail.com>
2014-06-12 22:03:08 +0100

    Revert "bugger..."
    This reverts commit cc9e12b578cc84395759353aaf9fc3c7f22cb08f.

---
+++ src/CQRS.Domain/Customer.cs
@@ -0,0 +1,44 @@
+﻿namespace CQRSPres.Domain
+{
+	public class Customer : Entity
+	{
+		protected Customer() {}
+		
+		public Customer(string email, string password)
+		{
+			// Null checks (or other domain validations)...
+
+			Email = email;
+			Password = password;
+		}
+
+		public Customer(string email, string password, string firstName, string lastName, int age, Address address)
+			: this(email, password)
+		{
+			// Null checks (or other domain validations)...
+
+			FirstName = firstName;
+			LastName = lastName;
+			Age = age;
+			Address = address;
+		}
+
+		public string FirstName { get; private set; }
+		public string LastName { get; private set; }
+		public string FullName { get { return FirstName + " " + LastName; } }
+		public int Age { get; private set; }
+		public string Email { get; private set; }
+		public string Password { get; private set; }
+		public Address Address { get; private set; }
+
+		public void ChangePassword(string password)
+		{
+			Password = password;
+		}
+
+		public void ChangeAddress(Address address)
+		{
+			Address = address;
+		}
+	}
+}
